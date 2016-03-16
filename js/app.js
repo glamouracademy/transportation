b09bf316169e1bd7bb0a9f55fe74c7f353db369b
Charlotte Chang <yellowspiderlove@gmail.com>
2016-03-16 11:15:20 -0400

    add toggle for icon and mobile menu

--- js/app.js
+++ js/app.js
@@ -1,19 +1,32 @@
 //Problem: mobile menu doesn't work
 //Solution: when the user taps the hamburger menu, the menu should show
-
+//implementation: http://jsfiddle.net/t7jtnupu/2/		
 var $closeIcon = $('<i class="fa fa-times fa-lg">');
 
-//when hamburger menu is tapped,
-$('.fa-bars').click(function(){
-//hide hamburger menu icon
-//display close icon
-	$(this).replaceWith($closeIcon);
-//display mobile menu
-$(".main-nav").show();
+// //when hamburger menu is tapped,
+// $('.fa-bars').click(function(){
+// //hide hamburger menu icon
+// //display close icon
+// 	$(this).replaceWith($closeIcon);
+// //display mobile menu
+// $(".main-nav").show();
+
+// });
+
+
+$('.mobile-menu').click(function() {
+	var collapsed = $(this).find('i').hasClass('fa fa-bars fa-lg');
+	$(this).find('i').removeClass('fa fa-times fa-lg');
+	$(this).find('i').addClass('fa fa-bars fa-lg');
+
+	if(collapsed) {
+		$(this).find('i').toggleClass('fa fa-bars fa-lg fa fa-times fa-lg');
+		$(".main-nav").show();
+	} else {
+		$(".main-nav").hide();
+	}
 
 });
-
-
 
 
 
