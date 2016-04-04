f9246d6a2e96a2efed3034a9ab967893f18a819e
Charlotte Chang <yellowspiderlove@gmail.com>
2016-04-04 11:22:20 -0400

    add jquery for just one animation

--- js/app.js
+++ js/app.js
@@ -94,31 +94,31 @@ $('nav.offering-nav').click(function(){
 
 
 //Offerings sliding in (desktop)
-// var $animation_element = $('.value.slide-left');
-// var $window = $(window);
+var $animation_element = $('.value.slide-left');
+var $window = $(window);
 
-// function check_if_in_view() {
-// 	var window_height = $window.height();
-// 	var window_top_position = $window.scrollTop();
-// 	var window_bottom_position = (window_top_position - window_height);
+function check_if_in_view() {
+	var window_height = $window.height();
+	var window_top_position = $window.scrollTop();
+	var window_bottom_position = (window_top_position - window_height);
 
-// 	$.each($animation_element, function() {
-// 		var $element = $(this);
-// 		var element_height = $element.outerHeight();
-// 		var element_top_position = $element.offset().top;
-// 		var element_bottom_position = (element_top_position - element_height);
+	$.each($animation_element, function() {
+		var $element = $(this);
+		var element_height = $element.outerHeight();
+		var element_top_position = $element.offset().top;
+		var element_bottom_position = (element_top_position - element_height);
 
-// 	//check to see if the current container is within viewport
-// 		if((element_bottom_position >= window_top_position) && (element_top_position <= window_bottom_position)) {
-// 			$element.addClass('in-view');
-// 		} else {
-// 			$element.removeClass('in-view');
-// 		}
-// 	});
-// }
+	//check to see if the current container is within viewport
+		if((element_bottom_position >= window_top_position) && (element_top_position <= window_bottom_position)) {
+			$element.addClass('in-view');
+		} else {
+			$element.removeClass('in-view');
+		}
+	});
+}
 
-// $window.on('scroll resize', check_if_in_view);
-// $window.trigger('scroll');
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
 
 
 
